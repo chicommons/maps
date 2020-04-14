6efc7e1c0e9f5643c7ab84e28819a3297b0c8c32
D. Alvarado <laredotornado@gmail.com>
2020-04-13 19:24:06 -0500

    Added test script.

---
+++ web/scripts/test_post_req.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+read -d '' req << EOF
+{
+        "name": "Test 8899",
+        "types": [
+            {"name": "Library"}
+        ],
+        "address": {
+            "formatted": "222 W. Merchandise Mart Plaza, Suite 1212",
+            "locality": {
+                "name": "Chicago",
+                "postal_code": "60654",
+                "state": 19313
+            }
+        },
+        "enabled": true,
+        "phone": "7739441426",
+        "email": "myemail@msn.com",
+        "web_site": "http://www.1871.com/"
+}
+EOF
+
+echo $req
+
+curl --header "Content-type: application/json" --data "$req" --request POST "http://localhost:8000/coops/"  
+ 
