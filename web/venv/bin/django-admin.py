77df351e45e27e09b01f8d3bd043830cb391eb15
Jerin Peter George <jerinpetergeorge@gmail.com>
2020-05-01 08:53:06 +0530

    venv directory removed

--- web/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/davea/Documents/workspace/chicommons/maps/web/venv/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
