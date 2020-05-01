edff9a2e5df35ab491e6481d3ddb509483a6fa62
D. Alvarado <laredotornado@gmail.com>
2020-04-30 20:56:27 -0500

    Adding tests.

---
+++ web/tests/test_models.py
@@ -0,0 +1,24 @@
+import pytest
+from django.test import TestCase
+from .tests.factories import CoopTypeFactory, CoopFactory
+
+
+class ModelTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        #management.call_command('loaddata', 'test_data.yaml', verbosity=0)
+        pass
+
+    def setUp(self):
+        print("setUp: Run once for every test method to setup clean data.")
+        #management.call_command('flush', verbosity=0, interactive=False)
+        pass
+
+    @pytest.mark.django_db
+    def test_coop_type_model():
+        """ Test customer model """    # create customer model instance
+        coop_type = CoopTypeFactory(name="Test Coop Type Name")
+        assert coop_type.name == "Test Coop Type Name"
+
+
