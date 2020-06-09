7c6805640dfc98e7a1b8ec63cea6d434c60fef29
D. Alvarado <laredotornado@gmail.com>
2020-06-09 13:59:09 -0500

    Added delete to init db script.

--- web/directory/management/commands/docker_init_db_data.py
+++ web/directory/management/commands/docker_init_db_data.py
@@ -1,14 +1,21 @@
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
-from address.models import Country
+from address.models import State, Country, Locality, Address
+from directory.models import Coop, CoopType
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not Country.objects.exists():
-            self.stdout.write('Seeding initial data')
-            call_command('loaddata', 'directory/fixtures/country_data.yaml')
-            call_command('loaddata', 'directory/fixtures/state_data.yaml')
-            call_command('loaddata', 'directory/fixtures/seed_data.yaml')
+        self.stdout.write('Deleting existing data')
+        CoopType.objects.all().delete()
+        Coop.objects.all().delete()
+        Address.objects.all().delete()
+        Locality.objects.all().delete()
+        State.objects.all().delete()
+        Country.objects.all().delete()
+        self.stdout.write('Seeding initial data')
+        call_command('loaddata', 'directory/fixtures/country_data.yaml')
+        call_command('loaddata', 'directory/fixtures/state_data.yaml')
+        call_command('loaddata', 'directory/fixtures/seed_data.yaml')
 
