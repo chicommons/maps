f723928e49b45452db695b60292d0fe2709383df
D. Alvarado <laredotornado@gmail.com>
2020-12-30 15:01:30 -0600

    Updated install script to prompt for Postgres username and exit if errors occur.

--- web/scripts/install_local.sh
+++ web/scripts/install_local.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -l
 
+set -e
+
 if [[ -z "${DB_NAME}" ]]; then
   echo "The environment variable DB_NAME is not defined."
   exit 1
@@ -21,7 +23,9 @@ if [[ -z "${DB_PORT}" ]]; then
   exit 1
 fi
 
-read -s -p "What is the root Postgres (user=postgres) password? "  ROOT_PASSWORD
+read -p "What is the root Postgres username [postgres]: " PG_USER
+PG_USER=${PG_USER:-postgres}
+read -s -p "What is the root Postgres (user=$PG_USER) password? "  ROOT_PASSWORD
 
 create_db_command="SELECT 'CREATE DATABASE $DB_NAME' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$DB_NAME')\gexec"
 create_user_command="create user $DB_USER with encrypted password '$DB_PASS';"
@@ -29,15 +33,15 @@ grant_privs_command="grant all privileges on database $DB_NAME to $DB_USER;"
 
 PGPASSWORD=$ROOT_PASSWORD 
 # This command creates the db if it doesn't already exist
-echo "SELECT 'CREATE DATABASE $DB_NAME' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$DB_NAME')\gexec" | psql -Upostgres
-PGPASSWORD=$ROOT_PASSWORD psql -Upostgres -c "$create_user_command" 
-PGPASSWORD=$ROOT_PASSWORD psql -Upostgres -c "$grant_privs_command" 
+echo "SELECT 'CREATE DATABASE $DB_NAME' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$DB_NAME')\gexec" | psql -U$PG_USER
+PGPASSWORD=$ROOT_PASSWORD psql -U$PG_USER -c "$create_user_command" 
+PGPASSWORD=$ROOT_PASSWORD psql -U$PG_USER -c "$grant_privs_command" 
 
 # Create Django environment, run migrations, and seed data
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPT_DIR
 cd ../
-if [ ! -d ".venv" ]; then
+if [ ! -d "venv" ]; then
   python3 -m venv venv
 fi
 # Activate virtual environment
