5bff6f21a0a03bf831037a39a2f3f79c1f669bf4
Nick Hou <hounicholas1@gmail.com>
2021-07-28 21:14:07 -0500

    added website footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = (props) => {
+  return (
+    <footer className="footer">
+      <div className="row justify-content-between footer-row">
+        <a href="https://www.chicommons.coop/privacy-policy/" className="credit-link">Privacy Policy</a>
+        <span>©  2021	Logo Copyright: ChiCommons LWCA</span>
+      </div>
+      <span>
+        Thanks to:
+        <a className="credit-link" href="https://github.com/laredotornado">Dave Alvarado</a>
+        |
+        <a className="credit-link" href="https://github.com/DaveyDevs">Davey Devs</a>
+        |
+        <a className="credit-link" href="https://github.com/ozzysChiefDataScientist">Elena Smith</a>
+        |
+        <a className="credit-link" href="https://github.com/nick-hou">Nick Hou</a>
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
