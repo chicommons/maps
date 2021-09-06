33f41b81ffac4a51828c1d24f9c5edb8f8cc156e
Elena Smith <elenasmith@Elenas-MacBook-Air.local>
2021-09-06 10:28:48 -0500

    Removed depracated function

--- client/src/__tests__/search.test.js
+++ client/src/__tests__/search.test.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {render, screen, waitForElement, waitFor} from '@testing-library/react'
+import {render, screen, waitFor} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import "mutationobserver-shim"
 import Search from '../components/Search'
