98ee9e004c7560761a12dfd797fed859ec610fed
D. Alvarado <laredotornado@gmail.com>
2021-10-23 12:26:22 -0500

    Added middleware to extend a token if necessary.

--- web/directory/settings.py
+++ web/directory/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'directory.middleware.extend_jwt_response.ExtendJWTResponse'
+    'directory.middleware.extend_token_response.ExtendTokenResponse'
 ]
 
 #CORS_ORIGIN_ALLOW_ALL = True
--- web/scripts/test_get_user_info.sh
+++ web/scripts/test_get_user_info.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #curl -i --header "Content-type: application/json" "http://localhost:8000/user_info"
-curl -i --header "Content-type: application/json" --header "Authorization: Token 7cbe1708b26de2d9f27b38699c83e8c1a7789e3d" "http://localhost:8000/user_info"
+curl -i --header "Content-type: application/json" --header "Authorization: Token dfd2d0e468373c8c14208b2795847cb723a5e6cf" "http://localhost:8000/user_info"
 
