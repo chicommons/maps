c0884d6bd50b2e7834561f5c36326696bc197640
D. Alvarado <laredotornado@gmail.com>
2021-12-13 10:07:20 -0600

    Added logic to ignore errors if creating super user and superuser already exists.

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -6,7 +6,7 @@ python manage.py migrate
 python manage.py migrate directory
 python manage.py insert_seed_data
 
-DJANGO_SUPERUSER_PASSWORD=password python manage.py createsuperuser --no-input --username=chicommons --email=chicommons@chicommons.com
+DJANGO_SUPERUSER_PASSWORD=password python manage.py createsuperuser --no-input --username=chicommons --email=chicommons@chicommons.com || true 
 
 exec "$@"
 
