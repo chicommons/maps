dc21110c86f2e016e1f7c28c980b90912bb84ce1
Kevin Jung <kev.jung.ii@gmail.com>
2022-05-12 18:54:49 -0500

    adds error handling for login and signup forms

--- client/src/components/NewUser.jsx
+++ client/src/components/NewUser.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FormGroup } from 'react-bootstrap';
-import { useHistory } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 import Input from './Input'
 import Button from './Button'
@@ -18,21 +18,16 @@ const NewUser = () => {
   const [passConfirm, setPassConfirm] = useState("")
   const [email, setEmail] = useState("")
 
-  //TODO: Implement validation errors
-  const [errors, setErrors, getErrors] = useState()
-
-  //history for redirecting user after submit
-  const history = useHistory()
+  const [errors, setErrors] = useState()
+  const [redirect, setRedirect] = useState(false)
 
   const handleFormSubmit = (e) => {
     e.preventDefault()
     if (password != passConfirm) {
       //handle password doesn't match password confirm on submit
-
+      setErrors({passConfirm: ["Must match password"]})
       return
     }
-
-
     fetch(REACT_APP_PROXY + '/users/', {
       method: 'POST',
       headers: {'Content-Type': 'application/json', 'Authorization': `Token ${sessionStorage.getItem('token')}`},
@@ -40,9 +35,9 @@ const NewUser = () => {
     }).then((response) => {
       if (response.ok) {
         sessionStorage.setItem('token', response['Refresh-Token'])
-        history.push('/signup')
+        setRedirect(true)
       } else {
-        //TODO:: Hanlde errors
+        setErrors(response)
         console.log(response)
       }
     })
@@ -57,75 +52,84 @@ const NewUser = () => {
       <h2 className="form__desc">
         <span style={{ color: "red" }}>*</span> = required
       </h2>
-      <FormGroup>
-        <div className="form-group col-md-8">
-          <Input
-            className={"required"}
-            type={"text"}
-            title={"First Name"}
-            name={"first_name"}
-            value={first_name}
-            placeholder={"First Name"}
-            handleChange={(e) => setFirstName(e.target.value)}
-          />
-        </div>
-        <div className="form-group col-md-8">
-          <Input
-            className={"required"}
-            type={"text"}
-            title={"Last Name"}
-            name={"last_name"}
-            value={last_name}
-            placeholder={"Last Name"}
-            handleChange={(e) => setLastName(e.target.value)}
-          />
-        </div>
-        <div className="form-group col-md-8">
-          <Input
-            className={"required"}
-            type={"text"}
-            title={"Username"}
-            name={"username"}
-            value={username}
-            placeholder={"Username"}
-            handleChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="form-group col-md-8">
-          <Input
-            className={"required"}
-            type={"password"}
-            title={"Password"}
-            name={"password"}
-            value={password}
-            placeholder={"Password"}
-            handleChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="form-group col-md-8">
-          <Input
-            className={"required"}
-            type={"password"}
-            title={"Confirm Password"}
-            name={"passConfirm"}
-            value={passConfirm}
-            placeholder={"Confirm Password"}
-            handleChange={(e) => setPassConfirm(e.target.value)}
-          />
-        </div>
-        <div className="form-group col-md-8">
-          <Input
-            className={"required"}
-            type={"text"}
-            title={"Email"}
-            name={"email"}
-            value={email}
-            placeholder={"Email"}
-            handleChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <Button action={handleFormSubmit} type={"primary"} title={"Sign Up"} />
-      </FormGroup>
+      <form
+      onSubmit={handleFormSubmit}
+      className="container-fluid"
+      id="login-form"
+      noValidate>
+        <FormGroup>
+          <div className="form-group col-md-8">
+            <Input
+              className={"required"}
+              type={"text"}
+              title={"First Name"}
+              name={"first_name"}
+              value={first_name}
+              placeholder={"First Name"}
+              handleChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="form-group col-md-8">
+            <Input
+              className={"required"}
+              type={"text"}
+              title={"Last Name"}
+              name={"last_name"}
+              value={last_name}
+              placeholder={"Last Name"}
+              handleChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div className="form-group col-md-8">
+            <Input
+              className={"required"}
+              type={"text"}
+              title={"Username"}
+              name={"username"}
+              value={username}
+              placeholder={"Username"}
+              handleChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="form-group col-md-8">
+            <Input
+              className={"required"}
+              type={"password"}
+              title={"Password"}
+              name={"password"}
+              value={password}
+              placeholder={"Password"}
+              handleChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="form-group col-md-8">
+            <Input
+              className={"required"}
+              type={"password"}
+              title={"Confirm Password"}
+              name={"passConfirm"}
+              value={passConfirm}
+              placeholder={"Confirm Password"}
+              handleChange={(e) => setPassConfirm(e.target.value)}
+            />
+          </div>
+          <div className="form-group col-md-8">
+            <Input
+              className={"required"}
+              type={"text"}
+              title={"Email"}
+              name={"email"}
+              value={email}
+              placeholder={"Email"}
+              handleChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group col-md-6" align="center">
+            <Button buttonType={"primary"} type={"submit"} title={"Sign Up"} />
+          </div>
+        </FormGroup>
+    </form>
+    {redirect ? <Redirect to="/" /> : null}
     </div>
   )
 }
