5631e1470b9fd72f657fe89a16bd57c97e5ebfc8
D. Alvarado <laredotornado@gmail.com>
2022-06-05 19:16:56 -0500

    Anotehr push test.

--- web/directory/authentication.py
+++ web/directory/authentication.py
@@ -4,7 +4,7 @@ from rest_framework.exceptions import AuthenticationFailed
 
 from datetime import timedelta
 from django.utils import timezone
-from django.conf import settings
+from directory import settings
 
 
 
