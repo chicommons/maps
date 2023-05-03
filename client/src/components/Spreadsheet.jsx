5b62abee41663ef6fb4e3e4813e4df96dc16dbaa
domdelorenzo <delorenzo.breed@gmail.com>
2023-05-02 19:46:11 -0500

    Adds new fields to spreadsheet. Removes ID fields.

--- client/src/components/Spreadsheet.jsx
+++ client/src/components/Spreadsheet.jsx
@@ -16,109 +16,131 @@ export default function Spreadsheet(props){
   const handleShow = () => setShow(true);
   const [allquery, setAllquery] = useState({})
 
-  const columnDefs= [
+  const columnDefs = [
     {name: 'id', header: 'ID', filter: 'select', sortable: true, resizable:true, width:70},
     {name: 'name', header: 'Name', filter: 'select', sortable: true, resizable:true, width:200},
     {name: 'phone', header: 'Phone', filter: 'select', sortable: true, resizable:true, width:200},
+    {name: 'phone_is_public', header: 'Phone is Public', filter: 'select', sortable: true, resizable:true, width:200},
     {name: 'email', header: 'Email', filter: 'select', sortable: true, resizable:true, width:200},
+    {name: 'email_is_public', header: 'Email is Public', filter: 'select', sortable: true, resizable:true, width:200},
     {name: 'web_site', header: 'Website', filter: 'select', sortable: true, resizable:true, width:200},
-    {name: 'street_number', header: 'Street Number', filter: 'select', sortable: true, resizable:true,width:100},
-    {name: 'route', header: 'Route', filter: 'select', sortable: true, resizable:true,width:100},
-    {name: 'raw', header: 'Raw', filter: 'select', sortable: true, resizable:true, width:200},
     {name: 'address', header: 'Address', filter: 'select', sortable: true, resizable:true, width:200},
     {name: 'latitude', header: 'Latitude', filter: 'select', sortable: true, resizable:true, width:100},
     {name: 'longitude', header: 'Longitude', filter: 'select', sortable: true, resizable:true, width:100},
-    {name: 'locality_id', header: 'Locality Id', filter: 'select', sortable: true, resizable:true,width:100},
     {name: 'city', header: 'City', filter: 'select', sortable: true, resizable:true, width:100},
     {name: 'state_id', header: 'State Id', filter: 'select', sortable: true, resizable:true, width:100},
-    {name: 'state_code', header: 'State Code', filter: 'select', sortable: true, resizable:true, width:100},
     {name: 'state', header: 'State', filter: 'select', sortable: true, resizable:true, width:100},
     {name: 'postal_code', header: 'Zip Code', filter: 'select', sortable: true, resizable:true,width:100},
-    {name: 'country_id',header: 'Country Id', filter: 'select', sortable: true, resizable:true,width:100},
     {name: 'country',header: 'Country', filter: 'select', sortable: true, resizable:true,width:100},
-    {name: 'country_code',header: 'Country Code', filter: 'select', sortable: true, resizable:true,width:100},
     {name: 'entity_types',header: 'Entity Type', filter: 'select', sortable: true, resizable:true,width:100},
     {name: 'country_code',header: 'Country Code', filter: 'select', sortable: true, resizable:true,width:100},
-    {name: 'is_public',header: 'Is Public?', filter: 'select', sortable: true, resizable:true,width:120},
+    {name: 'address_public',header: 'Address is Public?', filter: 'select', sortable: true, resizable:true,width:120},
     {name: 'approved',header: 'Approved?', filter: 'select', sortable: true, resizable:true,width:120},
+    {name: 'description',header: 'Entity Description', filter: 'select', sortable: true, resizable:true,width:120},
+    {name: 'reject_reason',header: 'Reject reason', filter: 'select', sortable: true, resizable:true,width:120},
+    {name: 'status',header: 'Status', filter: 'select', sortable: true, resizable:true,width:120},
+    {name: 'scope',header: 'Geographic Scope', filter: 'select', sortable: true, resizable:true,width:120},
+    {name: 'tags',header: 'Tags', filter: 'select', sortable: true, resizable:true,width:120},
+    {name: 'rec_source',header: 'Record source', filter: 'select', sortable: true, resizable:true,width:120},
+    {name: 'rec_updated_date"',header: 'Record updated date"', filter: 'select', sortable: true, resizable:true,width:120},
+    {name: 'rec_updated_by"',header: 'Record updated by', filter: 'select', sortable: true, resizable:true,width:120},
+
   ];
   
   const gridRef = useRef(null)
   
   const renderAllCoops = async () => {
-    await fetch(REACT_APP_PROXY + "/coops/all/")
-    .then((result) => {
-      return result.json()})
-    .then(data => renderSearchData(data))
+    try {
+      await fetch(REACT_APP_PROXY + "/coops/all/")
+      .then((result) => {
+        return result.json()})
+      .then(data => renderSearchData(data))
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   const renderSearchData = (data) => {
     data.forEach((obj)=>Object.keys(obj).forEach(
       (key) => (obj[key] === null) ? obj[key] = '' : obj[key]
     ))
-    const mapped = data.map(({
-      id, 
-      approved,
-      name, 
-      phone, 
-      email,
-      web_site,
-      types:[{
-        name:entity_types,
-      }],
-      coopaddresstags_set: [{ 
-        address: {
-          id:address_id,
-          street_number,
-          route,
-          raw,
-          formatted:formatted_address, 
-          latitude,
-          longitude,
-          locality: {
-            id:locality_id,
-            name:city, 
-            postal_code, 
-            state: { 
-              id:state_id,
-              code:state_code, 
-              name:state, 
-              country:{
-                id:country_id,
-                name:country,
-                code:country_code
-              } 
+    const mapped = data.map(
+      (
+        {
+          id, 
+          approved,
+          name, 
+          phone: [ phone = {}],
+          email: [ email = {}],
+          web_site,
+          types:[{
+            name:entity_types,
+          }],
+          coopaddresstags_set: [
+            { 
+              address: {
+                id:address_id,
+                formatted:formatted_address, 
+                latitude,
+                longitude,
+                locality: {
+                  name:city, 
+                  postal_code, 
+                  state: { 
+                    code:state_code, 
+                    name:state, 
+                    country:{
+                      id:country_id,
+                      name:country,
+                      code:country_code
+                    } 
+                  }
+                }
+              },
+              is_public: address_public,
+              description,
+              reject_reason,
+              scope,
+              tags,
+              rec_source,
+              rec_updated_by,
+              rec_updated_date,
             }
-          }
-        },
-        is_public,
-      }]
-    }) => ({ 
-      id,
-      approved, 
-      name,
-      street_number,
-      route,
-      raw,
-      address:formatted_address,
-      latitude,
-      longitude,
-      phone:phone.phone, 
-      email:email.email, 
-      web_site,
-      locality_id,
-      city,
-      state_id,
-      state_code, 
-      state, 
-      postal_code,
-      country_id,
-      country, 
-      country_code,
-      is_public,
-      entity_types
-    }));
-    setDataArray(mapped);
+          ]
+        }
+    ) => (
+      { 
+        id,
+        approved, 
+        name,
+        address:formatted_address,
+        latitude,
+        longitude,
+        phone: phone.phone,
+        phone_is_public:phone.phone_is_public, 
+        email:email.email,
+        email_is_public:email.email_is_public,
+        web_site,
+        city,
+        state_code, 
+        state, 
+        postal_code,
+        country_id,
+        country, 
+        country_code,
+        address_public,
+        entity_types,
+        description,
+        reject_reason,
+        scope,
+        tags,
+        rec_source,
+        rec_updated_by,
+        rec_updated_date,
+      }
+    )
+  );
+  setDataArray(mapped);
   }
 
   const cellClickedListener = useCallback( event => {
@@ -129,10 +151,17 @@ export default function Spreadsheet(props){
   useEffect(() => {
     if(props.searchResults){
       renderSearchData(props.searchResults)
+      console.log(props.searchResults)
     } else {
       renderAllCoops()
     }
   }, []);
+
+  const treeColumnOptions = {
+    // name: ['phone', 'email'],
+    name: 'phone',
+    useIcon: true,
+  }
 
   return(
     <div>
